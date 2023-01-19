9f21e9eda97ac2ebf1ae37c300cfb0d701cefa08
Tanveer Ahmad <tanveerisonline@gmail.com>
2023-01-19 15:01:56 +0530

    Add different components and setup the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Navbar from './components/Navbar';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import store from './redux/createState';
 import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route path="/categories">
+              <Categories />
+            </Route>
+            <Route exact path="/">
+              <Books />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
